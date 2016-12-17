82fdb07cb0457d690601c83faa3da8fcc5f225f1
Alexander Grebenschikov <me@package.su>
2016-12-17 10:57:20 +0300

    some beautify

--- frontend/templates/404.php
+++ frontend/templates/404.php
@@ -1,4 +1,4 @@
 <div class="content__title">Ошибка</div>
 <div class="content__text">
-	Страница не найдена
+  Страница не найдена
 </div>
--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
+date_default_timezone_set('Europe/Moscow');
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
 $pages = array(
-  "index" => "О кафедре",
+  "" => "О кафедре",
   "history" => "История кафедры",
   "partners" => "Предприятия партнеры",
   "tech" => "Материально-техническое обеспечение кафедры",
@@ -12,29 +16,25 @@ $pages = array(
 );
 
 $page = 404;
-$title = "Страница не найдена";
+$title = 'Страница не найдена';
 
 if (preg_match("/^\/([^?]*)/", $_SERVER["REQUEST_URI"], $matches)) {
-	$path = $matches[1];
-	if (empty($path)) {
-		$path = "index";
-	}
-
-	if (array_key_exists($path, $pages)) {
-		$page = $path;
-		$title = $pages[$path];
-	}
+  $path = $matches[1];
+  if (array_key_exists($path, $pages)) {
+    $page = $path;
+    $title = $pages[$path];
+  }
 }
 
 $params = array(
   "title" => $title,
   "page" => $page,
-  "content" => tpl($page)
+  "content" => tpl(empty($page) ? "index" : $page)
 );
 
 if (array_key_exists('HTTP_X_REQUESTED_WITH', $_SERVER)) {
-	header("Content-Type: application/json");
-	echo json_encode($params);
+  header("Content-Type: application/json");
+  echo json_encode($params);
 } else {
   $params['pages'] = $pages;
   echo tpl("layout", $params);
