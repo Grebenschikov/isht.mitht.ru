7452cf6ae476a697be3377e6889ed6cc80915265
Alexander Grebenschikov <me@package.su>
2017-02-21 13:55:15 +0300

    try import css

--- frontend/templates/layout.php
+++ frontend/templates/layout.php
@@ -80,8 +80,10 @@
     </span>
   </div>
 </div>
-<link href="/app.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600&subset=cyrillic-ext" rel="stylesheet">
+<style type="text/css">
+  @import(/app.css);
+  @import(https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600&subset=cyrillic-ext);
+</style>
 <!--[if lte IE 8]>
   <link rel="stylesheet" type="text/css" href="/ie.css" />
 <![endif]-->
